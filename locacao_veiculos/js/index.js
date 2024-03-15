50eec0be9ee8b1939e2bb299683f67b15325d470
EDUARDO RIBEIRO TORQUATO <eduardo.rtorquato@senacsp.edu.br>
2024-03-14 21:40:27 -0300

    banner/logo/carroussel

---
+++ locacao_veiculos/js/index.js
@@ -0,0 +1,44 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const idImg = document.getElementById('id_imagem');
+
+const idCompra = document.getElementById('id_comprar');
+
+const idAluga = document.getElementById('id_alugar');
+
+const idServicos = document.getElementById('id_servicos');
+
+const idAjuda = document.getElementById('id_ajuda');
+
+idImg.addEventListener('click', function(){
+    window.location.pathname="/locacao_veiculos/paginas/detalhes.html";
+} )
+
+idCompra.addEventListener('click', function(){
+    console.log('COMPRAR')
+ } );
+
+idAluga.addEventListener('click', function(){
+   console.log('ALUGAR')
+} )
+
+idAjuda.addEventListener('click', function(){
+   console.log('AJUDA')
+} )
+
+idServicos.addEventListener('click', function(){
+   console.log('SERVICOS')
+} )
+
