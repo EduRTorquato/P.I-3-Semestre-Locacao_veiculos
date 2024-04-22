42dd0f612a263c5daed26fab118a8b5051bd2ddf
EduRTorquato <eduardotorquato78@gmail.com>
2024-04-22 17:18:26 -0300

    esquema-telas

--- locacao_veiculos/js/conta.js
+++ locacao_veiculos/js/conta.js
@@ -1,8 +1,17 @@
 
 const id_alugados = document.getElementById("id_alugados");
 
+const id_sair = document.getElementById("id_sair");
+
+const id_favoritos = document.getElementById("favoritos");
+
 
 id_alugados.addEventListener('click', function () {
     window.location = "fluxo-conta-usuario/alugados.html";
 })
 
+id_sair.addEventListener("click", function () {
+
+    sessionStorage.clear;
+    window.location = "loginCadastro.html";
+})
--- locacao_veiculos/js/fluxo-usuario.js/alugados.js
+++ locacao_veiculos/js/fluxo-usuario.js/alugados.js
@@ -1,7 +1,16 @@
 const id_dados = document.getElementById("id_dados");
 
+const id_sair = document.getElementById("id_sair");
+
+
 
 id_dados.addEventListener("click", function () {
 
     window.location = "../conta.html";
+})
+
+id_sair.addEventListener("click", function () {
+
+    sessionStorage.clear;
+    window.location = "../loginCadastro.html";
 })
